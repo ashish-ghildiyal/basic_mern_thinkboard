f6a73c84f19352c6bae308670eb008657014c73f
Ashish <ashish.ghildiyal@xelba.io>
2025-08-29 13:10:49 +0530

    change to home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -48,7 +48,7 @@ const Home = () => {
         {isRateLimited && <RateLimitUI />}
         <div className="max-w-7xl mx-auto p-4 mt-6">
             {loading && <div className="text-center text-lg">Loading...</div>}
-           {notes.length==0 && !isRateLimited && <NotesNotFound/>}
+           {notes.length==0 && !loading && <NotesNotFound/>}
 
            {notes.length > 0 && !isRateLimited &&  (<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {
